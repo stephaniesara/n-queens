5f340095cef89f802eb263f410298ee2b5506ba8
Stephanie Sara Chong <stephaniesarachong@gmail.com>
2018-01-27 14:08:12 -0800

    Add tests for n up to 11 for nRooks and nQueens

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -22,10 +22,10 @@ describe('solvers', function() {
 
   describe('countNRooksSolutions()', function() {
 
-    it('finds the number of valid solutions for n of 1-8', function() {
-      _.range(1, 9).map(function(n) {
+    it('finds the number of valid solutions for n of 1-11', function() {
+      _.range(1, 11).map(function(n) {
         var solutionCount = countNRooksSolutions(n);
-        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
+        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800][n];
 
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
@@ -44,7 +44,7 @@ describe('solvers', function() {
             return memo + col;
           }, 0);
         }, 0);
-        
+
         expect(solutionBoard.get('n')).to.equal(n);
         expect(numPieces).to.equal(n);
         expect(solutionBoard.hasAnyQueensConflicts()).to.be.equal(false);
@@ -68,10 +68,10 @@ describe('solvers', function() {
 
   describe('countNQueensSolutions()', function() {
 
-    it('finds the number of valid solutions for n of 0-8', function() {
-      _.range(0, 9).map(function(n) {
+    it('finds the number of valid solutions for n of 0-11', function() {
+      _.range(0, 11).map(function(n) {
         var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
+        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724][n];
 
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
